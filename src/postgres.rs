46a47be0bfa737e02644de48f4d6a16b855e6877
Michael Cuffaro <consulting@michaelcuffaro.com>
2025-09-29 08:21:35 -0400

    implement query_row() and query() for postgres, and factor out extract_value()

--- src/postgres.rs
+++ src/postgres.rs
@@ -1,6 +1,6 @@
 use crate::core::{DbConnection, DbError, JsonRow, JsonValue};
 use deadpool_postgres::{Config, Pool, Runtime};
-use tokio_postgres::{types::Type, NoTls};
+use tokio_postgres::{row::Row, types::Type, NoTls};
 
 /// Represents a PostgreSQL database connection pool
 pub struct PostgresConnection {
@@ -19,9 +19,38 @@ impl PostgresConnection {
                 let pool = cfg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
                 Ok(Self { pool })
             }
-            // TODO: Replace panics and unwraps with proper errors, here and elsewhere.
+            // TODO: Replace panics and unwraps with proper errors, here and elsewhere in this
+            // module.
             false => panic!("Invalid PostgreSQL database path: '{url}'"),
         }
+    }
+}
+
+/// Extracts the value at the given index from the given [Row].
+fn extract_value(row: &Row, idx: usize) -> JsonValue {
+    let column = &row.columns()[idx];
+    match *column.type_() {
+        Type::TEXT | Type::VARCHAR => {
+            let value: &str = row.get(idx);
+            value.into()
+        }
+        Type::INT2 | Type::INT4 => {
+            let value: i32 = row.get(idx);
+            value.into()
+        }
+        Type::INT8 => {
+            let value: i64 = row.get(idx);
+            value.into()
+        }
+        Type::FLOAT4 => {
+            let value: f32 = row.get(idx);
+            value.into()
+        }
+        Type::FLOAT8 => {
+            let value: f64 = row.get(idx);
+            value.into()
+        }
+        _ => unimplemented!(),
     }
 }
 
@@ -34,60 +63,70 @@ impl DbConnection for PostgresConnection {
     }
 
     /// Implements [DbConnection::query()] for PostgreSQL.
-    async fn query(&self, _sql: &str, _params: &[JsonValue]) -> Result<Vec<JsonRow>, DbError> {
-        todo!()
+    async fn query(&self, sql: &str, _params: &[JsonValue]) -> Result<Vec<JsonRow>, DbError> {
+        let client = self.pool.get().await.unwrap();
+        let rows = client.query(sql, &[]).await.unwrap();
+        let mut json_rows = vec![];
+        for row in &rows {
+            let mut json_row = JsonRow::new();
+            let columns = row.columns();
+            for (i, column) in columns.iter().enumerate() {
+                json_row.insert(column.name().to_string(), extract_value(row, i));
+            }
+            json_rows.push(json_row);
+        }
+        Ok(json_rows)
     }
 
     /// Implements [DbConnection::query_row()] for PostgreSQL.
-    async fn query_row(&self, _sql: &str, _params: &[JsonValue]) -> Result<JsonRow, DbError> {
-        todo!()
+    async fn query_row(&self, sql: &str, _params: &[JsonValue]) -> Result<JsonRow, DbError> {
+        let client = self.pool.get().await.unwrap();
+        let rows = client.query(sql, &[]).await.unwrap();
+        if rows.is_empty() {
+            panic!("No rows found");
+        }
+        let row = rows[0].clone();
+        let mut json_row = JsonRow::new();
+        let columns = row.columns();
+        for (i, column) in columns.iter().enumerate() {
+            json_row.insert(column.name().to_string(), extract_value(&row, i));
+        }
+        Ok(json_row)
     }
 
     /// Implements [DbConnection::query_value()] for PostgreSQL.
     async fn query_value(&self, sql: &str, _params: &[JsonValue]) -> Result<JsonValue, DbError> {
         let client = self.pool.get().await.unwrap();
         let rows = client.query(sql, &[]).await.unwrap();
+        if rows.len() > 1 {
+            // TODO: Write a warning message to the log.
+        }
         match rows.iter().next() {
-            Some(row) => {
-                let column = &row.columns()[0];
-                match *column.type_() {
-                    Type::TEXT | Type::VARCHAR => {
-                        let value: &str = row.get(0);
-                        Ok(value.into())
-                    }
-                    Type::INT2 | Type::INT4 => {
-                        let value: i32 = row.get(0);
-                        Ok(value.into())
-                    }
-                    Type::INT8 => {
-                        let value: i64 = row.get(0);
-                        Ok(value.into())
-                    }
-                    Type::FLOAT4 => {
-                        let value: f32 = row.get(0);
-                        Ok(value.into())
-                    }
-                    Type::FLOAT8 => {
-                        let value: f64 = row.get(0);
-                        Ok(value.into())
-                    }
-                    _ => unimplemented!(),
-                }
-            }
-            None => panic!("No value found"),
+            Some(row) => Ok(extract_value(&row, 0)),
+            None => panic!("No rows found"),
         }
     }
 
     /// Implements [DbConnection::query_string()] for PostgreSQL.
     async fn query_string(&self, sql: &str, params: &[JsonValue]) -> Result<String, DbError> {
         let value = self.query_value(sql, params).await?;
-        Ok(value.to_string())
+        match value.as_str() {
+            Some(str_val) => Ok(str_val.to_string()),
+            None => {
+                // TODO (maybe?): Write a warning to the log that the value is not actually a
+                // string and that we are forcing it.
+                Ok(value.to_string())
+            }
+        }
     }
 
     /// Implements [DbConnection::query_u64()] for PostgreSQL.
     async fn query_u64(&self, sql: &str, _params: &[JsonValue]) -> Result<u64, DbError> {
         let client = self.pool.get().await.unwrap();
         let rows = client.query(sql, &[]).await.unwrap();
+        if rows.len() > 1 {
+            // TODO: Write a warning message to the log.
+        }
         match rows.iter().next() {
             Some(row) => {
                 let column = &row.columns()[0];
@@ -111,7 +150,7 @@ impl DbConnection for PostgresConnection {
                 };
                 Ok(value)
             }
-            None => panic!("No value found"),
+            None => panic!("No rows found"),
         }
     }
 
@@ -119,6 +158,9 @@ impl DbConnection for PostgresConnection {
     async fn query_i64(&self, sql: &str, _params: &[JsonValue]) -> Result<i64, DbError> {
         let client = self.pool.get().await.unwrap();
         let rows = client.query(sql, &[]).await.unwrap();
+        if rows.len() > 1 {
+            // TODO: Write a warning message to the log.
+        }
         match rows.iter().next() {
             Some(row) => {
                 let column = &row.columns()[0];
@@ -135,7 +177,7 @@ impl DbConnection for PostgresConnection {
                 };
                 Ok(value)
             }
-            None => panic!("No value found"),
+            None => panic!("No rows found"),
         }
     }
 
@@ -143,6 +185,9 @@ impl DbConnection for PostgresConnection {
     async fn query_f64(&self, sql: &str, _params: &[JsonValue]) -> Result<f64, DbError> {
         let client = self.pool.get().await.unwrap();
         let rows = client.query(sql, &[]).await.unwrap();
+        if rows.len() > 1 {
+            // TODO: Write a warning message to the log.
+        }
         match rows.iter().next() {
             Some(row) => {
                 let column = &row.columns()[0];
@@ -159,7 +204,7 @@ impl DbConnection for PostgresConnection {
                 };
                 Ok(value)
             }
-            None => panic!("No value found"),
+            None => panic!("No rows found"),
         }
     }
 }
@@ -167,9 +212,6 @@ impl DbConnection for PostgresConnection {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    // TODO: Once query_string(), query_i64(), query_f64, etc. have been implemented,
-    // replace the calls to query_value() below with the appropriate method.
 
     #[tokio::test]
     async fn it_works_with_text() {
@@ -185,14 +227,24 @@ mod tests {
         conn.execute("INSERT INTO test_table_text VALUES ('foo')", &[])
             .await
             .unwrap();
+        let select_sql = "SELECT value FROM test_table_text LIMIT 1";
         let value = conn
-            .query_value("SELECT value FROM test_table_text LIMIT 1", &[])
+            .query_value(select_sql, &[])
             .await
             .unwrap()
             .as_str()
             .unwrap()
             .to_string();
         assert_eq!("foo", value);
+
+        let value = conn.query_string(select_sql, &[]).await.unwrap();
+        assert_eq!("foo", value);
+
+        let row = conn.query_row(select_sql, &[]).await.unwrap();
+        assert_eq!(format!("{row:?}"), r#"{"value": String("foo")}"#);
+
+        let rows = conn.query(select_sql, &[]).await.unwrap();
+        assert_eq!(format!("{rows:?}"), r#"[{"value": String("foo")}]"#);
     }
 
     #[tokio::test]
@@ -206,16 +258,32 @@ mod tests {
         conn.execute("CREATE TABLE test_table_int ( value INT8 )", &[])
             .await
             .unwrap();
-        conn.execute("INSERT INTO test_table_int VALUES (-1)", &[])
+        conn.execute("INSERT INTO test_table_int VALUES (1)", &[])
             .await
             .unwrap();
+        let select_sql = "SELECT value FROM test_table_int LIMIT 1";
         let value = conn
-            .query_value("SELECT value FROM test_table_int LIMIT 1", &[])
+            .query_value(select_sql, &[])
             .await
             .unwrap()
             .as_i64()
             .unwrap();
-        assert_eq!(-1, value);
+        assert_eq!(1, value);
+
+        let value = conn.query_u64(select_sql, &[]).await.unwrap();
+        assert_eq!(1, value);
+
+        let value = conn.query_i64(select_sql, &[]).await.unwrap();
+        assert_eq!(1, value);
+
+        let value = conn.query_string(select_sql, &[]).await.unwrap();
+        assert_eq!("1", value);
+
+        let row = conn.query_row(select_sql, &[]).await.unwrap();
+        assert_eq!(format!("{row:?}"), r#"{"value": Number(1)}"#);
+
+        let rows = conn.query(select_sql, &[]).await.unwrap();
+        assert_eq!(format!("{rows:?}"), r#"[{"value": Number(1)}]"#);
     }
 
     #[tokio::test]
@@ -232,13 +300,25 @@ mod tests {
         conn.execute("INSERT INTO test_table_float VALUES (1.05)", &[])
             .await
             .unwrap();
+        let select_sql = "SELECT value FROM test_table_float LIMIT 1";
         let value = conn
-            .query_value("SELECT value FROM test_table_float LIMIT 1", &[])
+            .query_value(select_sql, &[])
             .await
             .unwrap()
             .as_f64()
             .unwrap();
-        let value = format!("{value:.2}");
+        assert_eq!("1.05", format!("{value:.2}"));
+
+        let value = conn.query_f64(select_sql, &[]).await.unwrap();
+        assert_eq!(1.05, value);
+
+        let value = conn.query_string(select_sql, &[]).await.unwrap();
         assert_eq!("1.05", value);
+
+        let row = conn.query_row(select_sql, &[]).await.unwrap();
+        assert_eq!(format!("{row:?}"), r#"{"value": Number(1.05)}"#);
+
+        let rows = conn.query(select_sql, &[]).await.unwrap();
+        assert_eq!(format!("{rows:?}"), r#"[{"value": Number(1.05)}]"#);
     }
 }
