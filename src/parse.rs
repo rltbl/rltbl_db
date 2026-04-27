af7cdade149ba4fd8f44bbe403bd0d72bfc9ea22
Michael Cuffaro <consulting@michaelcuffaro.com>
2026-04-27 07:57:19 -0400

    minor cleanup and documentation updates for parse.rs

--- src/parse.rs
+++ src/parse.rs
@@ -1,3 +1,5 @@
+//! Code for parsing SQL expressions.
+
 use crate::core::{DbError, QUERY_CACHE_TABLE, TABLE_CACHE_TABLE};
 use lazy_static::lazy_static;
 use regex::Regex;
@@ -5,16 +7,8 @@ use std::collections::HashSet;
 use tree_sitter::{Node, Parser};
 use tree_sitter_sequel::LANGUAGE as SQL_LANGUAGE;
 
-/// Represents a valid database table name.
-static VALID_TABLE_NAME_MATCH_STR: &str = r"^[A-Za-z_][0-9A-Za-z_]*$";
-
-lazy_static! {
-    /// The regex used to match [valid database table names](VALID_TABLE_NAME_MATCH_STR).
-    static ref VALID_TABLE_NAME_REGEX: Regex = Regex::new(VALID_TABLE_NAME_MATCH_STR).unwrap();
-}
-
 /// Validates that a given [Node] is not an error node:
-fn check_for_error(node: &Node<'_>, sql: &str) -> Result<(), DbError> {
+fn validate_node(node: &Node<'_>, sql: &str) -> Result<(), DbError> {
     if node.is_error() {
         return Err(DbError::ParseError(format!(
             "Error parsing '{sql}': {node}"
@@ -23,8 +17,8 @@ fn check_for_error(node: &Node<'_>, sql: &str) -> Result<(), DbError> {
     Ok(())
 }
 
-/// Checks that the given list of [Node]s is of the expected length:
-fn verify_list_len(node_list: &Vec<Node<'_>>, len: usize) -> Result<(), DbError> {
+/// Validates that the given list of [Node]s is of the expected length:
+fn validate_list_len(node_list: &Vec<Node<'_>>, len: usize) -> Result<(), DbError> {
     if node_list.len() != len {
         return Err(DbError::ParseError(format!(
             "Wrong number of values: {}. Expected: {}",
@@ -33,6 +27,14 @@ fn verify_list_len(node_list: &Vec<Node<'_>>, len: usize) -> Result<(), DbError>
         )));
     }
     Ok(())
+}
+
+/// Represents a valid database table name.
+static VALID_TABLE_NAME_MATCH_STR: &str = r"^[A-Za-z_][0-9A-Za-z_]*$";
+
+lazy_static! {
+    /// The regex used to match [valid database table names](VALID_TABLE_NAME_MATCH_STR).
+    static ref VALID_TABLE_NAME_REGEX: Regex = Regex::new(VALID_TABLE_NAME_MATCH_STR).unwrap();
 }
 
 /// Determines whether the given table name is a valid database table name. Valid database table
@@ -63,7 +65,7 @@ pub fn validate_table_name(table_name: &str) -> Result<String, DbError> {
 }
 
 /// Given `view_sql`, which is the SQL code that will result in the creation of a view, parse
-/// this code, determine what the view's source tables are, and return the list.
+/// it, determine what the view's source tables are, and return the list.
 pub fn get_view_tables(view_sql: &str) -> Result<Vec<String>, DbError> {
     // Instantiate the parser and try to parse the code:
     let mut parser = Parser::new();
@@ -78,7 +80,7 @@ pub fn get_view_tables(view_sql: &str) -> Result<Vec<String>, DbError> {
     // Collect the top-level statements:
     let statements = {
         let root_node = tree.root_node();
-        check_for_error(&root_node, &view_sql)?;
+        validate_node(&root_node, &view_sql)?;
         if root_node.kind().to_lowercase() != "program" {
             return Err(DbError::ParseError(format!(
                 "Unexpected root node kind: {}",
@@ -93,40 +95,40 @@ pub fn get_view_tables(view_sql: &str) -> Result<Vec<String>, DbError> {
 
     let mut view_tables = vec![];
     for statement in &statements {
-        check_for_error(&statement, &view_sql)?;
+        validate_node(&statement, &view_sql)?;
         for instruction in statement.children(&mut tree.walk()) {
-            check_for_error(&instruction, &view_sql)?;
+            validate_node(&instruction, &view_sql)?;
             match instruction.kind().to_lowercase().as_str() {
                 "create_view" => {
                     let create_query = instruction
                         .children(&mut instruction.walk())
                         .filter(|child| child.kind().to_lowercase() == "create_query")
                         .collect::<Vec<_>>();
-                    verify_list_len(&create_query, 1)?;
+                    validate_list_len(&create_query, 1)?;
                     let create_query = create_query[0];
-                    check_for_error(&create_query, &view_sql)?;
+                    validate_node(&create_query, &view_sql)?;
 
                     let from = create_query
                         .children(&mut create_query.walk())
                         .filter(|child| child.kind().to_lowercase() == "from")
                         .collect::<Vec<_>>();
-                    verify_list_len(&from, 1)?;
+                    validate_list_len(&from, 1)?;
                     let from = from[0];
-                    check_for_error(&from, &view_sql)?;
+                    validate_node(&from, &view_sql)?;
 
                     let relations = from
                         .children(&mut from.walk())
                         .filter(|child| child.kind().to_lowercase() == "relation")
                         .collect::<Vec<_>>();
                     for relation in relations {
-                        check_for_error(&relation, &view_sql)?;
+                        validate_node(&relation, &view_sql)?;
                         let object_ref = relation
                             .children(&mut relation.walk())
                             .filter(|child| child.kind().to_lowercase() == "object_reference")
                             .collect::<Vec<_>>();
-                        verify_list_len(&object_ref, 1)?;
+                        validate_list_len(&object_ref, 1)?;
                         let object_ref = object_ref[0];
-                        check_for_error(&object_ref, &view_sql)?;
+                        validate_node(&object_ref, &view_sql)?;
 
                         let source_table = view_sql.to_string()
                             [object_ref.start_byte()..object_ref.end_byte()]
@@ -144,12 +146,13 @@ pub fn get_view_tables(view_sql: &str) -> Result<Vec<String>, DbError> {
 
 /// Parse the given string, representing a series of (semi-colon-separated) SQL commands,
 /// into their constituents and determine the tables and views that will be affected when
-/// the commands are executed, if any. Two sets are returned. The first contains the tables
+/// the commands are executed, if any. Two sets are returned. The first contains tables
 /// and views that are going to be edited (targets of commands like INSERT, UPDATE, DELETE,
 /// ALTER, and TRUNCATE), the second contains tables and views that are going to be dropped
-/// (targets of a DROP TABLE command). All other kinds of statements will be silently ignored.
-/// In particular, table modifications that occur _within_ a CTE are not recognized by this
-/// function. Such table-modifying CTEs are supported by PostgreSQL (see
+/// (targets of a DROP TABLE command). All other kinds of statements are silently ignored.
+/// In particular, table modifications that occur _within_ a common table expression (CTE) are
+/// not recognized by this function. Such table-modifying CTEs are supported in principle by
+/// PostgreSQL (see
 /// <https://www.postgresql.org/docs/current/queries-with.html#QUERIES-WITH-MODIFYING>) but
 /// seemingly not by SQLite (see <https://sqlite.org/lang_with.html>).
 pub fn get_affected_tables(sql: &str) -> Result<(HashSet<String>, HashSet<String>), DbError> {
@@ -166,7 +169,7 @@ pub fn get_affected_tables(sql: &str) -> Result<(HashSet<String>, HashSet<String
     // Collect the top-level statements:
     let statements = {
         let root_node = tree.root_node();
-        check_for_error(&root_node, sql)?;
+        validate_node(&root_node, sql)?;
         if root_node.kind().to_lowercase() != "program" {
             return Err(DbError::ParseError(format!(
                 "Unexpected root node kind: {}",
@@ -195,9 +198,9 @@ pub fn get_affected_tables(sql: &str) -> Result<(HashSet<String>, HashSet<String
     let mut edited_tables = HashSet::new();
     let mut dropped_tables = HashSet::new();
     for statement in &statements {
-        check_for_error(&statement, sql)?;
+        validate_node(&statement, sql)?;
         for instruction in statement.children(&mut tree.walk()) {
-            check_for_error(&instruction, sql)?;
+            validate_node(&instruction, sql)?;
             match instruction.kind().to_lowercase().as_str() {
                 "insert" => {
                     let table_name = {
@@ -205,14 +208,14 @@ pub fn get_affected_tables(sql: &str) -> Result<(HashSet<String>, HashSet<String
                             .children(&mut instruction.walk())
                             .filter(|child| child.kind().to_lowercase() == "object_reference")
                             .collect::<Vec<_>>();
-                        verify_list_len(&object_ref, 1)?;
+                        validate_list_len(&object_ref, 1)?;
                         let object_ref = object_ref[0];
 
                         let identifier = object_ref
                             .children(&mut object_ref.walk())
                             .filter(|child| child.kind().to_lowercase() == "identifier")
                             .collect::<Vec<_>>();
-                        verify_list_len(&identifier, 1)?;
+                        validate_list_len(&identifier, 1)?;
                         let identifier = identifier[0];
 
                         validate_table_name(
@@ -227,21 +230,21 @@ pub fn get_affected_tables(sql: &str) -> Result<(HashSet<String>, HashSet<String
                             .children(&mut instruction.walk())
                             .filter(|child| child.kind().to_lowercase() == "relation")
                             .collect::<Vec<_>>();
-                        verify_list_len(&relation, 1)?;
+                        validate_list_len(&relation, 1)?;
                         let relation = relation[0];
 
                         let object_ref = relation
                             .children(&mut relation.walk())
                             .filter(|child| child.kind().to_lowercase() == "object_reference")
                             .collect::<Vec<_>>();
-                        verify_list_len(&object_ref, 1)?;
+                        validate_list_len(&object_ref, 1)?;
                         let object_ref = object_ref[0];
 
                         let identifier = object_ref
                             .children(&mut object_ref.walk())
                             .filter(|child| child.kind().to_lowercase() == "identifier")
                             .collect::<Vec<_>>();
-                        verify_list_len(&identifier, 1)?;
+                        validate_list_len(&identifier, 1)?;
                         let identifier = identifier[0];
 
                         validate_table_name(
@@ -263,14 +266,14 @@ pub fn get_affected_tables(sql: &str) -> Result<(HashSet<String>, HashSet<String
                             .children(&mut details.walk())
                             .filter(|child| child.kind().to_lowercase() == "object_reference")
                             .collect::<Vec<_>>();
-                        verify_list_len(&object_ref, 1)?;
+                        validate_list_len(&object_ref, 1)?;
                         let object_ref = object_ref[0];
 
                         let identifier = object_ref
                             .children(&mut object_ref.walk())
                             .filter(|child| child.kind().to_lowercase() == "identifier")
                             .collect::<Vec<_>>();
-                        verify_list_len(&identifier, 1)?;
+                        validate_list_len(&identifier, 1)?;
                         let identifier = identifier[0];
 
                         validate_table_name(
@@ -287,7 +290,7 @@ pub fn get_affected_tables(sql: &str) -> Result<(HashSet<String>, HashSet<String
                                 .children(&mut next_word.walk())
                                 .filter(|child| child.kind().to_lowercase() == "identifier")
                                 .collect::<Vec<_>>();
-                            verify_list_len(&identifier, 1)?;
+                            validate_list_len(&identifier, 1)?;
                             let identifier = identifier[0];
 
                             let table = validate_table_name(
@@ -304,14 +307,14 @@ pub fn get_affected_tables(sql: &str) -> Result<(HashSet<String>, HashSet<String
                             .children(&mut instruction.walk())
                             .filter(|child| child.kind().to_lowercase() == "object_reference")
                             .collect::<Vec<_>>();
-                        verify_list_len(&object_ref, 1)?;
+                        validate_list_len(&object_ref, 1)?;
                         let object_ref = object_ref[0];
 
                         let identifier = object_ref
                             .children(&mut object_ref.walk())
                             .filter(|child| child.kind().to_lowercase() == "identifier")
                             .collect::<Vec<_>>();
-                        verify_list_len(&identifier, 1)?;
+                        validate_list_len(&identifier, 1)?;
                         let identifier = identifier[0];
 
                         validate_table_name(
@@ -326,14 +329,14 @@ pub fn get_affected_tables(sql: &str) -> Result<(HashSet<String>, HashSet<String
                             .children(&mut instruction.walk())
                             .filter(|child| child.kind().to_lowercase() == "object_reference")
                             .collect::<Vec<_>>();
-                        verify_list_len(&object_ref, 1)?;
+                        validate_list_len(&object_ref, 1)?;
                         let object_ref = object_ref[0];
 
                         let identifier = object_ref
                             .children(&mut object_ref.walk())
                             .filter(|child| child.kind().to_lowercase() == "identifier")
                             .collect::<Vec<_>>();
-                        verify_list_len(&identifier, 1)?;
+                        validate_list_len(&identifier, 1)?;
                         let identifier = identifier[0];
 
                         validate_table_name(
@@ -348,14 +351,14 @@ pub fn get_affected_tables(sql: &str) -> Result<(HashSet<String>, HashSet<String
                             .children(&mut instruction.walk())
                             .filter(|child| child.kind().to_lowercase() == "object_reference")
                             .collect::<Vec<_>>();
-                        verify_list_len(&object_ref, 1)?;
+                        validate_list_len(&object_ref, 1)?;
                         let object_ref = object_ref[0];
 
                         let identifier = object_ref
                             .children(&mut object_ref.walk())
                             .filter(|child| child.kind().to_lowercase() == "identifier")
                             .collect::<Vec<_>>();
-                        verify_list_len(&identifier, 1)?;
+                        validate_list_len(&identifier, 1)?;
                         let identifier = identifier[0];
 
                         validate_table_name(
@@ -371,7 +374,7 @@ pub fn get_affected_tables(sql: &str) -> Result<(HashSet<String>, HashSet<String
     }
 
     // Edits of the cache tables themeselves are never cached so we do not need to report them.
-    // (However we do report a drop of the cache table.)
+    // Dropping one of the cache tables, by contrast, is always reported.
     let edited_tables = edited_tables
         .into_iter()
         .filter(|table| ![QUERY_CACHE_TABLE, TABLE_CACHE_TABLE].contains(&table.as_str()))
