7684e03a3e584caff5c9b80681dc7696bf8c3ec2
Michael Cuffaro <consulting@michaelcuffaro.com>
2026-09-13 12:11:19 -0400

    implement serialize_seq(), serialize_tuple()

--- src/serde.rs
+++ src/serde.rs
@@ -2,7 +2,7 @@
 
 use crate::{Error, JsonValue, Value};
 use serde::{Deserialize, Serialize, ser};
-use serde_json::{json, value::Serializer as JsonValueSerializer};
+use serde_json::{Map as JsonMap, json, value::Serializer as JsonValueSerializer};
 
 /// TODO: Add docstring.
 pub fn to_value<T>(value: &T) -> Result<Value, Error>
@@ -150,12 +150,27 @@ impl<'a> ser::Serializer for &'a mut ValueSerializer {
         }
     }
 
-    fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
-        todo!()
+    fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
+        // TODO: think about this. When would len be None? I guess this would be the case
+        // if the seq were an iterator, but I don't think we are going to support that.
+        let len = match len {
+            None => {
+                return Err(Error::SerdeError("Sequence length unknown".to_string()));
+            }
+            Some(len) => len,
+        };
+        if self.inner_value == JsonValue::Null {
+            self.remaining_fields = len;
+            // Start a new empty inner value which will be progressively filled in later:
+            self.inner_value = json!([]);
+            Ok(self)
+        } else {
+            Ok(self)
+        }
     }
 
     fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
-        todo!()
+        Ok(self)
     }
 
     fn serialize_struct(
@@ -333,15 +348,38 @@ impl<'a> ser::SerializeSeq for &'a mut ValueSerializer {
     type Ok = ();
     type Error = Error;
 
-    fn serialize_element<T>(&mut self, _value: &T) -> Result<(), Self::Error>
+    fn serialize_element<T>(&mut self, value: &T) -> Result<(), Self::Error>
     where
         T: ?Sized + Serialize,
     {
-        todo!()
+        if self.remaining_fields > 0 {
+            self.remaining_fields -= 1;
+            let json_serializer = JsonValueSerializer;
+            let json_value = value
+                .serialize(json_serializer)
+                .map_err(|err| Error::SerdeError(err.to_string()))?;
+            let inner = match self.inner_value.as_array_mut() {
+                Some(inner) => inner,
+                None => {
+                    return Err(Error::SerdeError(format!(
+                        "Not a JSON Array: {}",
+                        self.inner_value
+                    )));
+                }
+            };
+            inner.push(json_value);
+        } else {
+            value.serialize(&mut **self)?;
+        }
+        Ok(())
     }
 
     fn end(self) -> Result<(), Self::Error> {
-        todo!()
+        if self.inner_value != JsonValue::Null {
+            self.value = Value::Json(self.inner_value.clone());
+            self.inner_value = JsonValue::Null;
+        }
+        Ok(())
     }
 }
 
@@ -351,22 +389,107 @@ impl<'a> ser::SerializeMap for &'a mut ValueSerializer {
     type Ok = ();
     type Error = Error;
 
-    fn serialize_key<T>(&mut self, _key: &T) -> Result<(), Error>
+    fn serialize_key<T>(&mut self, key: &T) -> Result<(), Error>
     where
         T: ?Sized + Serialize,
     {
-        todo!()
+        // If this is the first entry, create a new inner map:
+        if self.inner_value == JsonValue::Null {
+            self.inner_value = json!({
+                "keys": [],
+                "values": [],
+            });
+        }
+        // Add the key to the keys list:
+        match self
+            .inner_value
+            .get_mut("keys")
+            .and_then(|k| k.as_array_mut())
+        {
+            Some(inner) => inner.push(json!(key)),
+            None => {
+                return Err(Error::SerdeError(format!(
+                    "Invalid inner value: {:?}",
+                    self.inner_value
+                )));
+            }
+        };
+        Ok(())
     }
 
-    fn serialize_value<T>(&mut self, _value: &T) -> Result<(), Error>
+    fn serialize_value<T>(&mut self, value: &T) -> Result<(), Error>
     where
         T: ?Sized + Serialize,
     {
-        todo!()
+        // Add the value to the values list:
+        match self
+            .inner_value
+            .get_mut("values")
+            .and_then(|v| v.as_array_mut())
+        {
+            Some(inner) => inner.push(json!(value)),
+            None => {
+                return Err(Error::SerdeError(format!(
+                    "Invalid inner value: {:?}",
+                    self.inner_value
+                )));
+            }
+        };
+        Ok(())
     }
 
     fn end(self) -> Result<(), Error> {
-        todo!()
+        let empty_vec = vec![];
+        let keys = {
+            if self.inner_value == JsonValue::Null {
+                &empty_vec
+            } else {
+                match self.inner_value.get("keys").and_then(|k| k.as_array()) {
+                    Some(inner) => inner,
+                    None => {
+                        return Err(Error::SerdeError(format!(
+                            "Invalid inner value: {:?}",
+                            self.inner_value
+                        )));
+                    }
+                }
+            }
+        };
+        let values = {
+            if self.inner_value == JsonValue::Null {
+                &empty_vec
+            } else {
+                match self.inner_value.get("values").and_then(|v| v.as_array()) {
+                    Some(inner) => inner,
+                    None => {
+                        return Err(Error::SerdeError(format!(
+                            "Invalid inner value: {:?}",
+                            self.inner_value
+                        )));
+                    }
+                }
+            }
+        };
+        if keys.len() != values.len() {
+            return Err(Error::SerdeError(format!(
+                "Inner keys and values have different lengths: \
+                 Keys: {keys:?} (length: {klen}), \
+                 Values: {values:?} (length: {vlen})",
+                klen = keys.len(),
+                vlen = values.len(),
+            )));
+        }
+        let mut json_map: JsonMap<String, JsonValue> = JsonMap::new();
+        for (i, key) in keys.iter().enumerate() {
+            let key = key
+                .as_str()
+                .ok_or(Error::SerdeError(format!("Not a string: {key}")))?
+                .to_string();
+            json_map.insert(key, values[i].clone());
+        }
+        self.value = Value::Json(json!(json_map));
+        self.inner_value = JsonValue::Null;
+        Ok(())
     }
 }
 
@@ -399,9 +522,13 @@ impl<'a> ser::SerializeStructVariant for &'a mut ValueSerializer {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::{HashMap, HashSet};
 
     #[test]
     fn test_serde() {
+        let value = to_value(&()).unwrap();
+        assert_eq!(value, Value::Null);
+
         let value = to_value(&1_i16).unwrap();
         assert_eq!(value, Value::SmallInteger(1));
 
@@ -413,5 +540,26 @@ mod tests {
 
         let value = to_value(&(1_i16, 3_u32)).unwrap();
         assert_eq!(value, Value::Json(json!([1_i16, 3_u32])));
+
+        let value = to_value(&[-11_i32, 20_i32]).unwrap();
+        assert_eq!(value, Value::Json(json!([-11_i32, 20_i32])));
+
+        let value = to_value(&vec![1_f32, 3_f32]).unwrap();
+        assert_eq!(value, Value::Json(json!([1_f32, 3_f32])));
+
+        let value = to_value::<Vec<u64>>(&vec![]).unwrap();
+        assert_eq!(value, Value::Json(json!([])));
+
+        let value = to_value::<HashSet<f64>>(&HashSet::new()).unwrap();
+        assert_eq!(value, Value::Json(json!([])));
+
+        let mut input = HashMap::new();
+        input.insert("foo".to_string(), true);
+        let value = to_value(&input).unwrap();
+        assert_eq!(value, Value::Json(json!({"foo": true})));
+
+        let input = HashMap::<String, u64>::new();
+        let value = to_value(&input).unwrap();
+        assert_eq!(value, Value::Json(json!({})));
     }
 }
